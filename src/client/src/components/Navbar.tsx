3140be9c94b2e5049a45c360da482d48eb5c917b
Mike <mhwilkes@gmail.com>
2020-10-06 10:52:21 -0400

    Navbar rework

--- src/client/src/components/Navbar.tsx
+++ src/client/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import auth from '../api/core/auth';
 
 const NavbarContainer = styled.div`
@@ -21,40 +21,83 @@ const NavbarItem = styled.div`
 `;
 
 const Navbar = () => {
-  let history = useHistory();
-  const [loggedIn, setLoggedIn] = useState(auth.isAuthenticated());
+    let history = useHistory();
+    const [loggedIn, setLoggedIn] = useState(auth.isAuthenticated());
 
-  const logout = ({ loggedIn }: { loggedIn: boolean }) => {
-    history.push('/');
-    setLoggedIn(loggedIn);
-  };
+    const logout = ({loggedIn}: { loggedIn: boolean }) => {
+        history.push('/');
+        setLoggedIn(loggedIn);
+    };
 
-  const login = ({ loggedIn }: { loggedIn: boolean }) => {
-    setLoggedIn(loggedIn);
-  };
+    const login = ({loggedIn}: { loggedIn: boolean }) => {
+        setLoggedIn(loggedIn);
+    };
 
-  auth.addLoginSubscribers(login);
-  auth.addLogoutSubscribers(logout);
+    auth.addLoginSubscribers(login);
+    auth.addLogoutSubscribers(logout);
 
-  return (
-    <NavbarContainer>
-      <NavbarItem>
-        <a href="/" className="title is-6 has-text-white">
-          🚀 TS-Boilerplate
-        </a>
-      </NavbarItem>
-      <NavbarItem>
-        {loggedIn && (
-          <h1
-            className="title is-6 has-text-white"
-            onClick={() => auth.logout()}
-          >
-            Logout
-          </h1>
-        )}
-      </NavbarItem>
-    </NavbarContainer>
-  );
+    return (
+        <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <img src="https://via.placeholder.com/112x28" width="112" height="28" alt="logo" />
+                </a>
+
+                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                   data-target="navbarBasicExample">
+                    <span aria-hidden="true" />
+                    <span aria-hidden="true" />
+                    <span aria-hidden="true" />
+                </a>
+            </div>
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <a
+                        className="navbar-item"
+                        onClick={() => {
+                            history.push('/');
+                        }}
+                    >
+                        Home
+                    </a>
+
+                    <a className="navbar-item">
+                        About
+                    </a>
+
+                </div>
+
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <h2
+                                className="button is-primary"
+                                onClick={() => history.push('/signup')}
+                            >
+                                Sign Up
+                            </h2>
+                            {loggedIn && (
+                                <h2
+                                    className="title is-6 has-text-white"
+                                    onClick={() => auth.logout()}
+                                >
+                                    Logout
+                                </h2>
+                            )}
+                            {!loggedIn && (
+                                <h2
+                                    className="button is-info"
+                                    onClick={() => history.push('/login')}
+                                >
+                                    Login
+                                </h2>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
